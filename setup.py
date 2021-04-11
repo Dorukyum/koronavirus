3887fa730cced3dbf4439c72e8a4fe397b258d9d
Dorukyum <doruk.ak@hotmail.com>
2021-04-11 14:31:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,24 @@
-from distutils.core import setup
+from setuptools import setup
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Developers",
+    "Topic :: Software Development :: Build Tools",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+]
+
 setup(
-  name = 'koronavirus',
-  packages = ['koronavirus'],
-  version = '0.1',
-  license='MIT',
-  description = 'Koronavirüs (Covid-19) verilerine erişmenizi sağlayan bir Python modülü.',
-  author = 'Dorukyum',
-  author_email = 'no.email@not.now.com',
-  url = 'https://github.com/Dorukyum/koronavirus',
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',
-  keywords = ['API', 'TÜRKÇE', 'COVID', 'KORONA'],
-  install_requires=["requests", "aiohttp"]
-  classifiers=[
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Developers',
-    'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-  ],
+    name="koronavirus",
+    packages=["koronavirus"],
+    version="0.0.1",
+    license="MIT",
+    description="Koronavirüs (Covid-19) verilerine erişmenizi sağlayan bir Python modülü.",
+    author="Dorukyum",
+    author_email="dorukyum@gmail.com",
+    url="https://github.com/Dorukyum/koronavirus",
+    download_url="https://github.com/user/reponame/archive/v_01.tar.gz",
+    keywords=["API", "Türkçe", "Covid", "Korona", "Corona"],
+    install_requires=["requests", "aiohttp"],
+    classifiers=classifiers,
 )
